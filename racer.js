f945fe71048c94c88fd12bacfc5c4089c3dd39fa
Loucky974 <clucie97495@gmail.com>
2019-11-20 12:00:43 -0300

    1essai

--- racer.js
+++ racer.js
@@ -6,7 +6,7 @@ window.addEventListener('keydown', function (event){
 // si la touche du clavier appuyée est la flèche gauche 
 if (event.key=="ArrowLeft"){
 // si la position à gauche est suppérieure à 0, on se déplace de 10 à droitz (-10 à gauche)
- $("#voiture").animate({
+ $("#vj").animate({
    left: "-=100"});
   }
    
@@ -15,7 +15,7 @@ if (event.key=="ArrowLeft"){
     if (event.key=="ArrowDown"){
     // si la position en bas est inférieure à la hauteur de la fenetre, on se déplace de 10 vers le bas (+10 en bas)
      
-      $("#voiture").animate({
+      $("#vj").animate({
         top: "+=100"});
       }
      
@@ -27,7 +27,7 @@ if (event.key=="ArrowLeft"){
 // si la position en haut est suppérieure à 0, on se déplace de 10 vers le haut (-10 en bas)
      
                
-                   $("#voiture").animate({
+                   $("#vj").animate({
                     top: "-=100"});
                   
            }
@@ -38,7 +38,7 @@ if (event.key=="ArrowLeft"){
            // si la position à droite est inférieur à la largeur de la page, on se déplace de 10 à gauche (+10 à gauche)
      
              
-              $("#voiture").animate({
+              $("#vj").animate({
                  left: "+=100"});
          
             
