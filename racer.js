a02225d14d4bd2d560442447124b9b37e0ca6301
Loucky974 <clucie97495@gmail.com>
2019-11-21 17:50:39 -0300

    essai4

--- racer.js
+++ racer.js
@@ -1,55 +1,43 @@
-$(document).keydown(function(e){
-  if (e.which==39)
-{
-  vjX = parseInt($('#vj').css('left'));
-  if (vjX < 280)
-    $('#vj').css('left', vjX+30);
-}
-
-if (e.which == 37)
-{
-  vjX = parseInt($('#vj').css('left'));
-  if (vjX > 70)
-    $('#vj').css('left', vjX-30);
-}
-});
-
-$(document).keydown(function(e){
-function deplace()
-{
-  $('#vr').animate({top: '-=600'}, 2500, 'linear', function(){
-    var vrX = Math.floor(Math.random()*194)+70;
-    var vrY = 400;
-
-    $(document).keydown(function(e){
-      if (e.which==39)
-{
-    $('#vr').css('top',vrY);
-
+  function deplace()
+  {
+    $('#vr').animate({top: '-=600'}, 2500, 'linear', function(){
+      var vrX = Math.floor(Math.random()*194)+70;
+      var vrY = 400;
+      $('#vr').css('top',vrY);
+      $('#vr').css('left',vrX);
+      //ok = 1;
+    });
+    $('.fond').animate({top: '-=360'}, 1000, 'linear', function(){
+      $('.fond').css('top',0);
+      deplace();
+    });
+  };
+ 
+  $(document).keydown(function(e){
+    if (e.which == 39)
+    {
+      vjX = parseInt($('#vj').css('left'));
+      if (vjX < 280)
+      $('#vj').css('left', vjX+30);
+    }
     if (e.which == 37)
-{
-    $('#vr').css('left',vrX);
-  ;}
-  ;}
-  }
+    {
+      vjX = parseInt($('#vj').css('left'));
+      if (vjX > 70)
+        $('#vj').css('left', vjX-30);
+    }
+  });
 
-    );}
-  )}
-});
+ 
   
 
 
 
-
-
-
-
-
-
-
-
-
-
+  $(document).ready(function() { 
+ 
+    deplace();
+   
+});
 
 
 
