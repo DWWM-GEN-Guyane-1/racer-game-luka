60ee4193eef168e61a6d85ca4431a5403333466a
Loucky974 <clucie97495@gmail.com>
2019-11-22 18:29:16 -0300

    essai6

--- racer.js
+++ racer.js
@@ -1,3 +1,6 @@
+
+
+  
   function deplace()
   {
     $('#vr').animate({top: '-=600'}, 2500, 'linear', function(){
@@ -5,7 +8,7 @@
       var vrY = 400;
       $('#vr').css('top',vrY);
       $('#vr').css('left',vrX);
-      //ok = 1;
+       ok = 1;
     });
     $('.fond').animate({top: '-=360'}, 1000, 'linear', function(){
       $('.fond').css('top',0);
@@ -28,16 +31,29 @@
     }
   });
 
- 
+  function collision()
+  {
+    vjX = parseInt($('#vj').css('left'));
+    vrX = parseInt($('#vr').css('left'));
+    vjY = 10;
+    vrY = parseInt($('#vr').css('top'));
+    if (((vrX > vjX) && (vrX < (vjX+66)) && (vrY > vjY) && (vrY < (vjY+150)) && (ok == 1)) 
+    || ((vjX > vrX) && (vjX < (vrX+66)) && (vrY > vjY) && (vrY < (vjY+150)) && (ok == 1)))
+  {
+      collision = parseInt($('#info').text()) + 1;
+      $('#info').text(collision);
+      ok = 0;
+    }  
+  }
   
 
 
 
   $(document).ready(function() { 
- 
+    var ok=1;
     deplace();
-   
-});
+    setInterval(collision, 20);
+  }  );
 
 
 
